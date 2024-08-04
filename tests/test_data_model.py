f9e191a5ee000e6ab814797f78d5d04e1b7849dc
Allen Porter <allen.porter@gmail.com>
2024-08-04 18:23:37 +0000

    Patch secrets when reading model cards

--- tests/test_data_model.py
+++ tests/test_data_model.py
@@ -1,6 +1,7 @@
 """Tests to validate the datamodel parsers."""
 
 import pathlib
+from unittest.mock import patch
 
 import pytest
 
@@ -10,7 +11,8 @@ from home_assistant_datasets import data_model
 
 def test_models() -> None:
     """Test the data_model model card parsing."""
-    assert data_model.read_models()
+    with patch("home_assistant_datasets.secrets.get_secret", return_value="SECRET"):
+        assert data_model.read_models()
 
 
 def test_dataset_cards() -> None:
