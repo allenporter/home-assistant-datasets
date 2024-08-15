34ac668cd6a29fe8c0b180d72380c65fc47e45d9
Allen Porter <allen.porter@gmail.com>
2024-08-15 05:18:54 +0000

    Set explicit snapshot directory

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+"""Test fixttures for home-assistant-datasets."""
+
+import pytest
+import pathlib
+
+from syrupy import SnapshotAssertion
+from syrupy.extensions.amber import AmberSnapshotExtension
+from syrupy.location import PyTestLocation
+
+
+DIFFERENT_DIRECTORY = "snapshots"
+
+
+class DifferentDirectoryExtension(AmberSnapshotExtension):
+    """Extension to set a different snapshot directory."""
+
+    @classmethod
+    def dirname(cls, *, test_location: "PyTestLocation") -> str:
+        """Override the snapshot directory name."""
+        return str(
+            pathlib.Path(test_location.filepath).parent.joinpath(DIFFERENT_DIRECTORY)
+        )
+
+
+@pytest.fixture
+def snapshot(snapshot: SnapshotAssertion):
+    """Fixture to override the snapshot directory."""
+    return snapshot.use_extension(DifferentDirectoryExtension)
