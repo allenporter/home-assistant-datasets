d84e62a14d13a4025f5fb76f39634932d58cdfd3
Allen Porter <allen.porter@gmail.com>
2025-07-06 00:43:15 +0000

    Update automations dataset reports

---
+++ script/eval_collect_automations.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# This script is used to evaluate the assist dataset using pytest.
+# The arguments are:
+#   - MODEL: The model to use for evaluation.
+
+set -e
+
+DATASET_NAME="automations"
+
+DATASET="datasets/${DATASET_NAME}/"
+if [ ! -d "$DATASET" ]; then
+  echo "Dataset directory ${DATASET} not found. DATASET_NAME environment variable to a valid path."
+  exit 1
+fi
+
+
+HA_VERSION=$(pip freeze | grep "^homeassistant==" | cut -f 3 -d '=')
+OUTPUT_DIR="reports/${DATASET_NAME}/${HA_VERSION}"  # Output based on home assistant version used
+
+if [ -z "$MODEL" ]; then
+  echo "MODEL is not set. Please set the MODEL environment variable."
+  exit 1
+fi
+if [ ! -d "home_assistant_datasets" ]; then
+    echo "home_assistant_datasets directory does not exist. Please run from repo root."
+    exit 1
+fi
+if [ ! -d "${DATASET}" ]; then
+  echo "Dataset directory ${DATASET} does not exist. Please check the dataset name."
+  exit 1
+fi
+if [ ! -d "${OUTPUT_DIR}" ]; then
+  echo "Output directory ${OUTPUT_DIR} does not exist. Creating it."
+  mkdir -p "${OUTPUT_DIR}"
+fi
+
+# Ensure the synthetic home component is setup
+SYNTHETIC_HOME_DIR="/workspaces/home-assistant-synthetic-home/"
+if [ ! -d "${SYNTHETIC_HOME_DIR}" ]; then
+  echo "Synthetic home directory ${SYNTHETIC_HOME_DIR} does not exist. Please check the path."
+  exit 1
+fi
+export PYTHONPATH="${PYTHONPATH}:${SYNTHETIC_HOME_DIR}"
+
+pytest home_assistant_datasets/tool/automation/collect --models=${MODEL} --dataset=${DATASET} --model_output_dir=${OUTPUT_DIR}
