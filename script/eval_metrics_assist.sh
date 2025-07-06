3318123d130abfece0e2dac97449ffb8533f2e9e
Allen Porter <allen.porter@gmail.com>
2025-07-06 00:22:58 +0000

    Update assist-mini and questions datasets

--- script/eval_metrics_assist.sh
+++ script/eval_metrics_assist.sh
@@ -6,7 +6,16 @@
 
 set -e
 
-DATASET_NAME="assist"
+if [ -z "$DATASET_NAME" ]; then
+  echo "DATASET_NAME is not set. Please set the DATASET_NAME environment variable."
+  exit 1
+fi
+
+DATASET="datasets/${DATASET_NAME}/"
+if [ ! -d "$DATASET" ]; then
+  echo "Dataset directory ${DATASET} not found. DATASET_NAME environment variable to a valid path."
+  exit 1
+fi
 
 HA_VERSION=$(pip freeze | grep "^homeassistant==" | cut -f 3 -d '=')
 OUTPUT_DIR="reports/${DATASET_NAME}/${HA_VERSION}"  # Output based on home assistant version used
